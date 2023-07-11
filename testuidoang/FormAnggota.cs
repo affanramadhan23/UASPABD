dbf3ee1246f49d49b3331c8de15efd31511cfd81
affanramadhan23 <affannabilramadhan@gmail.com>
2023-07-11 11:39:20 +0700

    add database connection form anggota

--- testuidoang/FormAnggota.cs
+++ testuidoang/FormAnggota.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace testuidoang
 {
     public partial class FormAnggota : Form
     {
+        private string stringConnection = "data source=LAPTOP-C3M8HP9E\\KURANGTAU;" + "database=perpustakaan`; user ID=sa;Password=affancool23";
+        private SqlConnection koneksi;
+
         public FormAnggota()
         {
             InitializeComponent();
